1624c6b22372d1d64ef77e7a3231d56105c6add5
余聪 <yucong@yuanfudao.com>
2021-11-05 17:14:07 +0800

    chore: fix confluence

--- src/shared/parse-html-treenode.ts
+++ src/shared/parse-html-treenode.ts
@@ -2,23 +2,29 @@ import TreeNode from './tree-node'
 
 const isBlockElem = (elem) => ['DIV', 'P', 'BR'].includes(elem?.tagName)
 
-function generateText(domElem: Element) {
+function generateText(domElem: Node) {
   if (!domElem) {
     return ''
   }
-
-  if (domElem.tagName === 'S' || domElem.tagName === 'DEL') {
+  if (domElem.nodeType === Node.TEXT_NODE) {
+    return domElem.textContent
+  }
+  if (domElem.nodeType !== Node.ELEMENT_NODE) {
+    return ''
+  }
+  const elem = domElem as HTMLElement
+  if (elem.tagName === 'S' || elem.tagName === 'DEL') {
     return ''
   }
 
-  const suffix = isBlockElem(domElem) ? '\n' : ''
+  const suffix = isBlockElem(elem) ? '\n' : ''
 
-  if (!domElem.childElementCount) {
-    return (domElem.textContent || '') + suffix
+  if (!elem.childElementCount) {
+    return (elem.textContent || '') + suffix
   }
 
   let text = ''
-  for (const childElem of domElem.children) {
+  for (const childElem of elem.childNodes) {
     text += generateText(childElem)
   }
   return text + suffix
@@ -26,9 +32,27 @@ function generateText(domElem: Element) {
 
 function generateTreeNode(domElem, parentTreeNode?: TreeNode) {
   if (domElem.tagName === 'LI') {
+    domElem = domElem.cloneNode(true)
+    const iter = document.evaluate('./ul/li|./ol/li', domElem, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+    const tNode = new TreeNode()
+    let li
+    let i = 0
+    while (i < iter.snapshotLength) {
+      const node = iter.snapshotItem(i)
+      if (node) {
+        tNode.addNode(generateTreeNode(node))
+      }
+      if (node && !li) {
+        li = node
+      }
+      i++
+    }
+    li?.parentElement?.remove()
+
     const text = generateText(domElem).trim()
     if (text) {
-      return new TreeNode(text)
+      tNode.value = text
+      return tNode
     }
     return null
   }
@@ -49,10 +73,7 @@ function generateTreeNode(domElem, parentTreeNode?: TreeNode) {
   return null
 }
 
-
-export default function parseHtmlTreeNode(
-  html: string,
-): false | TreeNode {
+export default function parseHtmlTreeNode(html: string): false | TreeNode {
   const docElement = new DOMParser().parseFromString(html, 'text/html')
   const ulElements = docElement.querySelectorAll('[data-shimo-docs] > ul, body > ul')
   if (!ulElements.length) {
